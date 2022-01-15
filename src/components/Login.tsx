139477745dff459320100c40d86e5e990e8cb086
Michał <michal.dros01@gmail.com>
2022-01-15 13:57:55 +0100

    Add base login file

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from "react";
+import styles from "./Login.module.scss";
+
+
+const Login: FC = () => {
+    return (
+        <div className={"login-page"}>
+
+        </div>
+    );
+}
