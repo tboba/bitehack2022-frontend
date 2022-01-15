e13a25232c0bb08afdb6f2f95aa2dda4724f13a9
DESKTOP-1J0O10P\Lenovo <krystiandzik06@gmail.com>
2022-01-15 18:57:24 +0100

    Add product list item styling

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -9,32 +9,42 @@ interface MarkerProps {
   lng: number;
 }
 
-const Marker: FC<MarkerProps> = (props) => <div><MoreIcon/></div>;
+const Marker: FC<MarkerProps> = (props) => <div><MoreIcon /></div>;
 
 const Map: FC = () => {
 
-    const coordinates = { lat: 0, lng : 0};
-    return (
-      <div className={styles.mapContainer}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAPjaMdeqXnLNpwS6uXha3duczlHZDIlT8' }}
-          defaultCenter={coordinates}
-          center={coordinates}
-          defaultZoom={14}
-          margin={[50, 50, 50, 50]}
-          options={{ disableDefaultUI: true, zoomControl: true }}
-        >
-          <Marker
-              lat={59.955413}
-              lng={30.337844} 
-            />
-            <Marker
-              lat={0}
-              lng={0} 
-            />
-        </GoogleMapReact>
-      </div>
-    )
+
+  // GET CURRENT LOCATION
+  // const coordinates = { lat: 52.40050890143135, lng : 16.909968708057384};
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(pos => showPosition(pos));
+  // }
+
+  // function showPosition(position:any) {
+  //   console.warn(position.coords.latitude,position.coords.longitude)
+  // }
+
+  return (
+    <div className={styles.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAPjaMdeqXnLNpwS6uXha3duczlHZDIlT8' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true }}
+      >
+        <Marker
+          lat={59.955413}
+          lng={30.337844}
+        />
+        <Marker
+          lat={0}
+          lng={0}
+        />
+      </GoogleMapReact>
+    </div>
+  )
 }
 
 export default Map;
--- src/components/product-list-item/ProductListItem.tsx
+++ src/components/product-list-item/ProductListItem.tsx
@@ -14,7 +14,12 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import TimelapseOutlinedIcon from '@mui/icons-material/TimelapseOutlined';
 
+import styles from './ProductListItem.module.scss';
+import { Box } from '@mui/material';
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
@@ -39,78 +44,44 @@ export default function ProductListItem() {
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://mlv36vxt8ewe.i.optimole.com/-kj5tHQ-sLt5KVwi/w:720/h:720/q:75/rt:fill/g:sm/https://hiszpanskiesmaki.pl/wp-content/uploads/2020/08/Paella-mixta.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+      <Box sx={{ position: 'relative' }}>
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://mlv36vxt8ewe.i.optimole.com/-kj5tHQ-sLt5KVwi/w:720/h:720/q:75/rt:fill/g:sm/https://hiszpanskiesmaki.pl/wp-content/uploads/2020/08/Paella-mixta.jpg"
+          alt="Paella dish"
+        />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            padding: '10px',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          <div className={styles.badge} title={'Time left'}>
+            <TimelapseOutlinedIcon />&nbsp;23:45
+          </div>
+        </Box>
+      </Box>
+      <CardContent className={styles.titleContainer}>
+        <Typography variant="h6" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" className={styles.descriptionContainer}>
+          Lizard test as dsadsad sad saokdosak ods   asdasd sadsa
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
+      <CardActions disableSpacing className={styles.actionsContainer}>
+        <div className={styles.badge} title={"Distance from your location"}>
+          <LocationOnOutlinedIcon />&nbsp;150m
+        </div>
+        <div className={styles.badge} title={'Time since added'}>
+          <AccessTimeOutlinedIcon />&nbsp;2d
+        </div>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    </Card >
   );
 }
