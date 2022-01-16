3d42a90bb3cab4c381e95a6902f3750c0af36d72
DESKTOP-1J0O10P\Lenovo <krystiandzik06@gmail.com>
2022-01-16 10:48:31 +0100

    Fix profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -5,31 +5,24 @@ import FaceIcon from '@mui/icons-material/Face';
 import styles from "./Profile.module.scss"
 import ProductListItem from "../product-list-item/ProductListItem";
 import ProductsList from "../products-list/ProductsList";
+import { MapItem } from "../map/MapItem";
+import { useAppSelector } from "../../store/store-hooks";
 
 const Profile = () => {
+    const posts = useAppSelector(state => state.posts.posts) || [];
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className={styles.info}>
-                <FaceIcon className={styles.avatarIcon}/>
+                <FaceIcon className={styles.avatarIcon} />
                 <div className={styles.accountInfo}>
-                    <h2>Michal Dros</h2>
-                    <h3>test@test.com</h3>
+                    <h2>John Doe</h2>
+                    <h3>johndoe@gmail.com</h3>
                 </div>
             </div>
             <div className={styles.items}>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
-                <ProductListItem canBeDeleted={true}/>
+                {posts.map((post: MapItem) => <ProductListItem mapItem={post} canBeDeleted={true} />)}
             </div>
         </div>
     );
